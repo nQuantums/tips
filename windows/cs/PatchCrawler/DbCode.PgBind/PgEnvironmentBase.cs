545f46bea2d5987305d51f4d85c0d12eef5d3c49
nQuantums <nquantums@gmail.com>
2018-05-22 23:11:49 +0900

    プライマリキー無しの状態でテーブルデルタ求めるとnull参照で落ちる不具合修正

--- windows/cs/PatchCrawler/DbCode.PgBind/PgEnvironmentBase.cs
+++ windows/cs/PatchCrawler/DbCode.PgBind/PgEnvironmentBase.cs
@@ -126,9 +126,11 @@ namespace DbCode.PgBind {
 					} else if (pka == null && pkt != null) {
 						tableDelta.PrimaryKeyToAdd = pkt;
 					} else {
-						if (pka.Name != pkt.Name || !EqualColumnDefs(pka.Columns, pkt.Columns)) {
-							tableDelta.PrimaryKeyToDrop = pka;
-							tableDelta.PrimaryKeyToAdd = pkt;
+						if (pka != null) {
+							if (pka.Name != pkt.Name || !EqualColumnDefs(pka.Columns, pkt.Columns)) {
+								tableDelta.PrimaryKeyToDrop = pka;
+								tableDelta.PrimaryKeyToAdd = pkt;
+							}
 						}
 					}
 
