47e1ddb94ff48ea5fb29f7482c72037633ffa840
nQuantums <nquantums@gmail.com>
2019-02-15 02:04:44 +0900

    インストールコンポーネント列挙してみた

--- windows/cpp/RegSnapshot/RegSnapshot.cpp
+++ windows/cpp/RegSnapshot/RegSnapshot.cpp
@@ -4,12 +4,32 @@
 #include "pch.h"
 #include <Windows.h>
 #include <iostream>
+#include <string>
+#include <locale>
+#include <codecvt>
+#include <fstream>
 #include <vector>
 #include <string>
 
 std::wstring subkeyName;
 std::wstring valueName;
 std::vector<uint8_t> valueData;
+std::wstring valueSz;
+std::ofstream* pOutputFile;
+
+std::string utf16_to_utf8(const std::wstring& str) {
+	static_assert(sizeof(wchar_t) == 2, "this function is windows only");
+	const int len = ::WideCharToMultiByte(CP_UTF8, 0, str.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	std::string re(len * 2, '\0');
+	if (!::WideCharToMultiByte(CP_UTF8, 0, str.c_str(), -1, &re[0], len, nullptr, nullptr)) {
+		const auto er = ::GetLastError();
+		throw std::system_error(std::error_code(er, std::system_category()), "WideCharToMultiByte:(" + std::to_string(er) + ')');
+	}
+	const std::size_t real_len = std::char_traits<char>::length(re.c_str());
+	re.resize(real_len);
+	re.shrink_to_fit();
+	return re;
+}
 
 void EnumerateRegKeys(HKEY hKey) {
 	DWORD cSubKeys, cchMaxSubKeyName, cValues, cchMaxValueName, cbMaxValueData;
@@ -37,15 +57,10 @@ void EnumerateRegKeys(HKEY hKey) {
 			cchMaxValueName++;
 		}
 
-		if (valueName.size() < cchMaxValueName) {
-			valueName.resize(cchMaxValueName);
-		}
-
-		if (valueData.size() < cbMaxValueData) {
-			valueData.resize(cbMaxValueData);
-		}
-
 		for (DWORD i = 0; ; i++) {
+			valueName.resize(cchMaxValueName);
+			valueData.resize(cbMaxValueData);
+
 			DWORD cchValueName, nValueType, cbValueData;
 			cchValueName = (DWORD)valueName.size();
 			cbValueData = (DWORD)valueData.size();
@@ -66,9 +81,18 @@ void EnumerateRegKeys(HKEY hKey) {
 				continue;
 			}
 
-			valueName[cchValueName] = 0;
-	
-			std::wcout << valueName << std::endl;
+			valueName.resize(cchValueName);
+
+			if (nValueType == REG_SZ && (lstrcmpW(valueName.c_str(), L"DisplayName") == 0 || lstrcmpW(valueName.c_str(), L"DisplayVersion") == 0 || lstrcmpW(valueName.c_str(), L"Publisher") == 0)) {
+				auto strSize = (cbValueData + 1) / 2;
+				valueSz.resize(0);
+				valueSz.resize(strSize);
+				memcpy_s((void*)valueSz.data(), cbValueData, valueData.data(), cbValueData);
+				*pOutputFile << "	- " << utf16_to_utf8(valueName) << " : " << utf16_to_utf8(valueSz) << std::endl;
+			} else if (nValueType == REG_DWORD && lstrcmpW(valueName.c_str(), L"Language") == 0) {
+				auto value = *(DWORD*)valueData.data();
+				*pOutputFile << "	- " << utf16_to_utf8(valueName) << " : " << value << std::endl;
+			}
 		}
 	}
 
@@ -77,13 +101,10 @@ void EnumerateRegKeys(HKEY hKey) {
 			cchMaxSubKeyName++;
 		}
 
-		if (subkeyName.size() < cchMaxSubKeyName) {
-			subkeyName.resize(cchMaxSubKeyName);
-		}
-
 		for (DWORD i = 0; ; i++) {
-			DWORD cchSubKeyName;
+			subkeyName.resize(cchMaxSubKeyName);
 
+			DWORD cchSubKeyName;
 			cchSubKeyName = (DWORD)subkeyName.size();
 			nErrNo = ::RegEnumKeyExW(
 				hKey, i,
@@ -100,12 +121,12 @@ void EnumerateRegKeys(HKEY hKey) {
 				// TODO: process/protocol issue in some way, do not silently ignore it (at least in Debug builds)
 				continue;
 			}
-			subkeyName[cchSubKeyName] = 0;
+			subkeyName.resize(cchSubKeyName);
 
-			std::wcout << subkeyName << std::endl;
+			*pOutputFile << "# " << utf16_to_utf8(subkeyName) << std::endl;
 		
 			HKEY hRegSubKey;
-			nErrNo = ::RegOpenKeyExW(hKey, subkeyName.data(), 0, KEY_QUERY_VALUE | KEY_ENUMERATE_SUB_KEYS, &hRegSubKey);
+			nErrNo = ::RegOpenKeyExW(hKey, subkeyName.data(), 0, KEY_QUERY_VALUE | KEY_ENUMERATE_SUB_KEYS | KEY_WOW64_64KEY, &hRegSubKey);
 			if (ERROR_SUCCESS != nErrNo) {
 				continue;
 			}
@@ -117,7 +138,25 @@ void EnumerateRegKeys(HKEY hKey) {
 
 
 int main() {
-	EnumerateRegKeys(HKEY_LOCAL_MACHINE);
+	//std::locale::global(std::locale(""));
+	//std::wcout.imbue(std::locale("Japanese", LC_COLLATE));
+	//std::wcout.imbue(std::locale("Japanese", LC_CTYPE));
+
+	std::ofstream ofs("output.md");
+	pOutputFile = &ofs;
+
+	HKEY hRegSubKey;
+	auto result = ::RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_QUERY_VALUE | KEY_ENUMERATE_SUB_KEYS | KEY_WOW64_64KEY, &hRegSubKey);
+	if (result == ERROR_SUCCESS) {
+		EnumerateRegKeys(hRegSubKey);
+	}
+
+	result = ::RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, KEY_QUERY_VALUE | KEY_ENUMERATE_SUB_KEYS | KEY_WOW64_64KEY, &hRegSubKey);
+	if (result == ERROR_SUCCESS) {
+		EnumerateRegKeys(hRegSubKey);
+	}
+
+	ofs.close();
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
